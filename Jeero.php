65724e5f0ecf780cd59db893a301f095dfc6d4f9
Jeroen Schmit <jeroen@slimndap.com>
2020-09-15 15:14:15 +0200

    Fixed version number.

--- Jeero.php
+++ Jeero.php
@@ -5,7 +5,7 @@
  * Description:     Synchronizes events and tickets from your existing ticketing solution with popular calendar plugins.
  * Author:          Slim & Dapper
  * Author URI:      https://slimndap.com
- * Version:         1.0.4
+ * Version:         1.1
  * Text Domain: 	jeero
  *
  * @package         Jeero
