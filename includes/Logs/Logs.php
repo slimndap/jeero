67ccd9964e65c661e1bb9f2bae5656e0221da9c3
Jeroen Schmit <jeroen@slimndap.com>
2022-08-29 15:04:17 +0200

    Added support for importing server side logs.

--- includes/Logs/Logs.php
+++ includes/Logs/Logs.php
@@ -11,6 +11,12 @@ const MAX_LOG_FILESIZE = 5242880; //Max filesize in bytes (e.q. 5MB)
 const LOG_UID_KEY = 'jeero_log_uid_key';
 
 /**
+ * Attaches log import to inbox filter.
+ * @since	1.18.1
+ */
+add_filter( 'jeero/inbox/process/item/log', __NAMESPACE__.'\log_from_inbox', 10, 5 );
+
+/**
  * Creates the upload dir for the Jeero log file.
  * 
  * @since	1.18
@@ -25,7 +31,9 @@ function create_upload_dir() {
 		return $upload_dir_path;
 	}
 	
-	mkdir( $upload_dir_path, 0700, true );
+	if ( !wp_mkdir_p( $upload_dir_path ) ) {
+		return new WP_Error( 'logs', sprintf( 'Unable to create %s folder for Jeero log', $upload_dir_path ) );
+	}
 	
 	$index_php_path = $upload_dir_path.'index.php';
 	$index_php_content = '<?php // Silence is golden';
@@ -80,6 +88,11 @@ function get_uid() {
 function get_file_path() {
 
 	$upload_dir = get_upload_dir();
+	
+	if ( \is_wp_error(  $upload_dir ) ) {
+		return $upload_dir;
+	}
+	
 	return sprintf( '%s%s.log', $upload_dir, get_uid() );
 	
 }
@@ -94,6 +107,10 @@ function rotate_logs() {
 	
 	$file_path = get_file_path();
 	
+	if ( \is_wp_error( $file_path ) ) {
+		return;
+	}
+	
 	if ( !file_exists( $file_path ) ) {
 		return;
 	}
@@ -107,7 +124,7 @@ function rotate_logs() {
 }
 
 /**
- * Logs a message in the Jeero logfile.
+ * Logs a message to the Jeero logfile.
  * 
  * @since	1.18
  * @param 	string	$message
@@ -119,9 +136,36 @@ function log( $message ) {
 	
 	$file_path = get_file_path();
 	
+	if ( \is_wp_error( $file_path ) ) {
+		return $file_path;
+	}
+	
 	$message = sprintf( "[%s] %s\n", date( 'r' ), $message );
 	
 	error_log( $message, 3, $file_path );
+	
+}
+
+/**
+ * Logs a message from the inbox in the Jeero logfile.
+ * 
+ * @since	1.18.1
+ */
+function log_from_inbox( $result, $data, $raw, $theater, $subscription ) {
+	
+	if ( is_wp_error( $result ) ) {
+		return $result;
+	}
+	
+	$message = $data[ 'message' ];
+	
+	if ( !empty( $theater ) ) {
+		$message = sprintf( '[%s] %s', $theater, $message );
+	}
+	
+	log( $message );
+	
+	return $result;
 	
 }
 
@@ -134,6 +178,10 @@ function log( $message ) {
 function get_log_file_content() {
 	
 	$file_path = get_file_path();
+	
+	if ( \is_wp_error( $file_path ) ) {
+		return $file_path->get_error_message();
+	}
 	
 	if ( !file_exists( $file_path ) ) {
 		return '';
