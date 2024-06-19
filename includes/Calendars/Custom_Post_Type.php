a5e9b10cbf33ff4150033b9f1442b3ce97963b6c
Jeroen Schmit <jeroen@slimndap.com>
2024-06-19 10:56:05 +0200

    Added support for hierarchial posts in CTP import.

--- includes/Calendars/Custom_Post_Type.php
+++ includes/Calendars/Custom_Post_Type.php
@@ -22,8 +22,60 @@ class Custom_Post_Type extends Post_Based_Calendar {
 
 	}
 	
+	function get_post_fields() {
+				
+		$post_fields = parent::get_post_fields();
+		
+		$filtered_post_fields = array();
+		
+		foreach( $post_fields as $post_field ) {
+			
+			if ( 'content' == $post_field[ 'name' ] ) {
+				
+				if ( !\post_type_supports( $this->get_post_type(), 'editor' ) ) {
+					continue;
+				}
+				
+			}
+			
+			if ( 'excerpt' == $post_field[ 'name' ] ) {
+				
+				if ( !\post_type_supports( $this->get_post_type(), 'excerpt' ) ) {
+					continue;
+				}
+				
+			}
+			
+			$filtered_post_fields[] = $post_field;
+			
+		}
+	
+		return $filtered_post_fields;
+	
+	}
+			
+	function get_post_ref( $data ) {
+
+		$ref = parent::get_post_ref( $data );
+
+		if ( !$this->use_event_dates() ) {
+			return $ref;
+		}
+		
+		if ( !empty( $data[ 'production' ][ 'ref' ] ) ) {
+			$ref = $data[ 'production' ][ 'ref' ];
+		} else {
+			$ref = 'e'.$ref;		
+		}
+
+		return $ref;
+				
+	}
+		
 	function get_setting_fields( $subscription ) {
 		
+		$this->post_type = $this->get_setting( 'import/custom_post_type', $subscription );
+
 		$fields = parent::get_setting_fields( $subscription );
 		
 		$filtered_fields = array();
@@ -43,7 +95,7 @@ class Custom_Post_Type extends Post_Based_Calendar {
 					''
 				);
 				foreach( $post_types as $post_type )	 {
-					$choices[ $post_type->name ] = $post_type->label;
+					$choices[ $post_type->name ] = sprintf( '%s (%s)', $post_type->label, $post_type->name );
 				}
 				
 				$filtered_fields[] = array(
@@ -51,7 +103,6 @@ class Custom_Post_Type extends Post_Based_Calendar {
 					'label' => __( 'Custom Post Type', 'jeero' ),
 					'type' => 'select',
 					'choices' => $choices,
-					'instructions' => __( 'Eg. events for the same movie or events that are part of a festival.', 'jeero' ),
 				);
 			}
 		}
@@ -66,21 +117,91 @@ class Custom_Post_Type extends Post_Based_Calendar {
 
 		$result = parent::process_data( $result, $data, $raw, $theater, $subscription );
 
-		$event_id = $this->get_event_by_ref( $data[ 'ref' ], $theater );		
+		$event_id = $this->get_event_by_ref( $this->get_post_ref( $data ), $theater );		
 		if ( !$event_id ) {
 			$this->log( sprintf( 'Import of event %s skipped: no existing post found.', $data[ 'ref' ] ) );
+			return $result;
 		}
 		
 		$menu_order = strtotime( $data[ 'start' ] );
 
-		$event_args = array(
-			'ID' => $event_id,
-			'menu_order' => $menu_order,					
-		);
-		\wp_update_post( $event_args );
+		if ( $this->use_event_dates() ) {
+
+			$this->log( sprintf( '%s can have parent-child relationships.', $this->get_post_type() ) );
+			
+			$date_ref = $data[ 'ref' ];
+			$ref_key = $this->get_ref_key( $theater );
+
+			$args = array(
+				'post_type' => $this->get_post_type(),
+				'post_status' => array( 'publish', 'draft' ),
+				'meta_query' => array(
+					array(
+						'key' => $ref_key,
+						'value' => $date_ref,
+					),
+				),
+			);			
+			$posts = \get_posts( $args );
+
+			$date_args = array(
+				'post_title' => $data[ 'production' ][ 'title'],
+				'post_type' => $this->get_post_type(),
+				'post_content' => '',		
+				'post_excerpt' => '',
+				'post_parent' => $event_id,
+				'menu_order' => $menu_order,					
+			);
+			
+			if ( empty( $posts ) ) {
+				
+				$this->log( sprintf( 'Creating date item %s for event item %d.', $date_ref, $event_id ) );
+				
+				$date_args[ 'post_status' ] = 'publish';
+				
+				$date_id = \wp_insert_post( $date_args, true );
+	
+				if ( \is_wp_error( $date_id ) ) {
+					return $date_id;
+				}
+				
+				\add_post_meta( $date_id, $ref_key, $date_ref, true );
+				
+			} else {
+
+				$date_id = $posts[ 0 ]->ID;
+				
+				$this->log( sprintf( 'Updating date item %d for event item %d.', $date_id, $event_id ) );
+
+				$date_args[ 'ID' ] = $date_id;
+				
+				\wp_update_post( $date_args );
+				
+			}
+			
+		} else {
+
+			$this->log( sprintf( '%s can not have parent-child relationships.', $this->get_post_type() ) );
+
+				
+			$event_args = array(
+				'ID' => $event_id,
+				'menu_order' => $menu_order,					
+			);
+			\wp_update_post( $event_args );
+			
+		}
 
 		return $result;
 
+	}
+
+	function use_event_dates() {
+		
+		$post_type_object = get_post_type_object( $this->get_post_type() );
+		
+		return $post_type_object && $post_type_object->hierarchical;
+		
 	}
 	
 }
