85f8e5652ddc497b4682fd07009b727cd0aa1f74
Jeroen Schmit <jeroen@slimndap.com>
2024-11-23 12:40:10 +0100

    Fixed missing custom post type settings tab if no post type was selected yet.

--- includes/Calendars/Custom_Post_Type.php
+++ includes/Calendars/Custom_Post_Type.php
@@ -113,14 +113,15 @@ class Custom_Post_Type extends Post_Based_Calendar {
 					'choices' => $choices,
 				);
 				
+				// Remove all other fields if no post type is selected yet.
+				if ( empty( $this->get_post_type() ) ) {
+					break;
+				}
+
 				continue;
 				
 			}
 			
-			// Remove all other fields if no post type is selected yet.
-			if ( empty( $this->get_post_type() ) ) {
-				break;
-			}
 
 			// Prepend 'upcate/categories' field with categories taxonomy dropdown.
 			if ( sprintf( '%s/import/update/categories', $this->slug ) == $field[ 'name' ] ) {
