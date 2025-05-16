3a2944c203d61795f76888679920a9d099f54e44
Jeroen Schmit <jeroen@slimndap.com>
2025-05-16 21:07:32 +0200

    Move constant definitions inside the 'init' action for proper initialization

--- Jeero.php
+++ Jeero.php
@@ -18,11 +18,13 @@ if ( ! defined( 'ABSPATH' ) ) {
     return;
 }
 
-define( 'Jeero\VERSION', '1.31.1' );
-define( 'Jeero\PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
-define( 'Jeero\PLUGIN_URI', plugin_dir_url( __FILE__ ) );
 
 // Bootstrap plugin on init: include core functionality.
 add_action( 'init', function() {
+
+    define( 'Jeero\VERSION', '1.31.1' );
+    define( 'Jeero\PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+    define( 'Jeero\PLUGIN_URI', plugin_dir_url( __FILE__ ) );
+    
     include_once \Jeero\PLUGIN_PATH . 'includes/Jeero.php';
 }, 5 );
